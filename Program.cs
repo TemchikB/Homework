a6cfa8ff50d7f4efbd481c831f385368dcc7dd5f
temcik <strong.balkizov@mail.ru>
2023-12-13 10:32:37 +0300

    fillarray done

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
-﻿Console.WriteLine("Введите  колчиество слов которое хотите вписать")
+﻿Console.WriteLine("Введите  колчиество слов которое хотите вписать");
 int n =Convert.ToInt32(Console.ReadLine());
 string [] inarray = new string [n];
+Fillarray();
+void Fillarray(){
+for (int i =0; i< inarray.Length;i++){
+    inarray[i] = Console.ReadLine();
+}
+}
